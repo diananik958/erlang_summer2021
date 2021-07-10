ea7030f98c6e847165aa838c175d8c48bea10dc6
diananik958 <66783977+diananik958@users.noreply.github.com>
2021-07-11 00:18:15 +0700

    Add files via upload

---
+++ task1.erl
@@ -0,0 +1,15 @@
+-module(task1).
+-export([open_file/1]).
+-export([parse_data/1]).
+
+%"Desktop/Erlang/data.csv"
+
+open_file(File) ->
+    {ok, Data}=file:read_file(File),
+    io:format(Data).
+    parse_data(Data).
+
+parse_data(Data) ->
+    
+
+    file:close(Data).
