815c634467b3c6a47c4a29ec57b83103ef06085d
diananik958 <66783977+diananik958@users.noreply.github.com>
2021-07-12 23:27:01 +0700

    Add files via upload

---
+++ t1map.erl
@@ -0,0 +1,8 @@
+ -module(t1map).
+-export([info/0]).
+
+
+info() ->
+    Map = #{first=>{mike, hemings, 24, +76547893456}, second=>{nina, black, 20, +78767658945}},
+    io:fwrite("The person is "),
+    maps:get(first, Map).
