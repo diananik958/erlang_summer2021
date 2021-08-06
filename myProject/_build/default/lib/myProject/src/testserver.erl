906af37c23e941b88c5bea2fd37d1f9aa2888048
diananik958 <66783977+diananik958@users.noreply.github.com>
2021-08-06 08:51:57 +0700

    Add files via upload

--- myProject/_build/default/lib/myProject/src/testserver.erl
+++ myProject/_build/default/lib/myProject/src/testserver.erl
@@ -2,6 +2,28 @@
 -export([main/0, client/1, get_file/1]).
 -compile({no_auto_import,[error/1]}).
 
+
+formatstring(Elem) ->
+    IOList = io_lib:format("~w", [Elem]),
+    FlatList = lists:flatten(IOList) -- "[",
+    FlatList -- "]".
+
+
+mysql(Tuple) ->
+    {ok, Pid} = mysql:start_link([{host, "localhost"}, {user, "test"},
+                                {password, "Test123!"}, {database, "testserver1"}]),
+    {{ok, {IPshort, Port}}, {{Year,Month,Day},{Hour,Min,Sec}}, {abs_path, Path}, Method} = Tuple,
+    io:fwrite("Path for mysql: ~p~n", [Path]),
+    D = formatstring(Year) ++ "-" ++ formatstring(Month) ++ "-" ++ formatstring(Day),
+    io:fwrite("Date ~p~n", [D]),
+    T = formatstring(Hour) ++ ":" ++ formatstring(Min) ++ ":" ++ formatstring(Sec),
+    io:fwrite("Time ~p~n", [T]),
+    %% Manipulate data
+    ok = mysql:query(Pid, "INSERT INTO server VALUES (?, ?, ?, ?, ?, ?)", [formatstring(IPshort), Port, D, T, Path, formatstring(Method)]),
+    io:fwrite("OKKK"),
+    mysql:stop(Pid).
+
+
 read_file(Fname) ->
     {ok, File} = file:read_file(Fname),
     File.
@@ -40,11 +62,17 @@ send_file(ReqPath) ->
 
 client(Socket) ->
     {ok, {http_request, Method, Path, _Version}} = gen_tcp:recv(Socket, 0),
+    io:fwrite("Method: ~p~nPath: ~p~nVersion: ~p~n", [Method, Path, _Version]),
     Html = send_file(Path),
+    IP = inet:peername(Socket),
     gen_tcp:send(Socket, Html),
     gen_tcp:close(Socket),
     case (Method) of
         'GET' ->
+            DateTime = erlang:localtime_to_universaltime(erlang:localtime()), %forman: {1996,11,6},{14,45,17}
+            io:fwrite("IP: ~p~n", [IP]),
+            Tuple = {IP, DateTime, Path, Method},
+            mysql(Tuple),
             handle_get(Path);
         _ -> 
             error(500)
@@ -58,5 +86,5 @@ server(ServerSocket) ->
 
 
 main() ->
-    {ok, ServerSocket} = gen_tcp:listen(8080, [binary, {active, false}, {packet, http}, {reuseaddr, true}]),
+    {ok, ServerSocket} = gen_tcp:listen(8080, [list, {active, false}, {packet, http}, {reuseaddr, true}]),
     server(ServerSocket).
